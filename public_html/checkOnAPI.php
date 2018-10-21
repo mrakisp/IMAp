1f4646b8cc153e98995258c5e0dddb0934f4f474
paneras <panerasakis@gmail.com>
2018-10-21 13:38:16 +0300

    new born

---
+++ public_html/checkOnAPI.php
@@ -0,0 +1,37 @@
+<?php
+
+$search_val = str_replace(' ', '+', $_GET["ls_query"]);
+$api_key = "apikey=401c0211";
+$search_term = $search_val . '&' . $api_key;
+$url = 'http://www.omdbapi.com/?t=' . $search_term . ''; // path to your JSON file
+$data = file_get_contents($url); // put the contents of the file into a variable
+$characters = json_decode($data); // decode the JSON feed
+
+
+if (isset($characters->Title)) {
+    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "g13g31g31g_movies";
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO live_search_table (name) VALUES ('$characters->Title')";
+        // use exec() because no results are returned
+        $conn->exec($sql);
+        
+        echo "New record created successfully";
+    } catch (PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+
+    $conn = null;
+
+
+    
+}
+
+?>
